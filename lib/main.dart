48daa275226c84e88bf9374e16e0f88be69423a1
Rodriguez, Marc Adrian C. <marcishere04@gmail.com>
2025-06-08 21:25:59 +0800

    Firebase Integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:integriscan/screens/welcome/welcome_screen.dart';
 import 'package:integriscan/constant.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const IndexPage());
 }
+
+
 
 class IndexPage extends StatelessWidget {
   const IndexPage({super.key});
